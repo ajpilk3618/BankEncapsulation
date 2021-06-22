6bd7853804bd5f2efd343cf64533a777f6715ea4
AJ Pilkerton <apilkerton3618@gmail.com>
2021-06-22 14:14:20 -0500

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+
+        public void Withdraw()
+        {
+            Console.Write("Please enter amount to be withdrawn: ");
+            double amount = double.Parse(Console.ReadLine());
+            Console.Write("Current Balance: ");
+            Console.WriteLine(balance -= amount);
+        }
+
+        public void Deposit()
+        {
+            Console.Write("Please enter amount to be deposited: ");
+            double amount = double.Parse(Console.ReadLine());
+            Console.Write("Current Balance: ");
+            Console.WriteLine(balance += amount);
+        }
+
+        public void GetBalance()
+        {
+            Console.Write("Current Balance: ");
+            Console.WriteLine(balance);
+        }
+
+        public void SignOut()
+        {
+            Console.WriteLine("Good Bye!");
+        }
+
+    }
+}
---
+++ UserInterface.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class UserInterface : BankAccount
+    {
+        public void Start()
+        {
+
+            bool loggedIn = true;
+
+            while (loggedIn)
+            {
+                BankAccount account = new BankAccount();
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("A: Withdraw");
+                Console.WriteLine("B: Deposit");
+                Console.WriteLine("C: Check Balance");
+                Console.WriteLine("D: Sign Out");
+                var userResponse = Console.ReadLine();
+
+                switch (userResponse)
+                {
+                    case "A":
+                        //Withdraw
+                        {
+                            Withdraw();
+                            Console.WriteLine("Press Enter to continue");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        }
+                    case "B":
+                        //Deposit
+                        {
+                            Deposit();
+                            Console.WriteLine("Press Enter to continue");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        }
+                    case "C":
+                        //Check Balance
+                        {
+                            GetBalance();
+                            Console.WriteLine("Press Enter to continue");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        }
+                    case "D":
+                        //Sign Out
+                        {
+                            SignOut();
+                            loggedIn = false;
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
+
